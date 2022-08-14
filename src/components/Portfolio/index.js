52055142d926a59d61c4ef0b331127e07e7180de
Mark Pavel <markpavel02@gmail.com>
2022-08-14 18:35:01 -0400

    Added Project Component And Portfolio Content

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,59 @@
 import React from "react";
+import Project from "../Project";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 export default function Portfolio() {
-    return(
-        <div>
-            Portfolio
-        </div>
+    const projects = [
+        {
+            title: 'Rippit',
+            link: 'https://github.com/ProgrammingAStorm/rippit',
+            image: require('../../assets/images/rippit.jpg'),
+            deploy: 'https://ancient-retreat-86124.herokuapp.com'
+        },
+        {
+            title: 'Budget Tracker',
+            link: 'https://github.com/ProgrammingAStorm/budget-tracker',
+            image: require('../../assets/images/budget-tracker.jpg'),
+            deploy: 'https://mysterious-depths-45527.herokuapp.com/index.html'
+        },
+        {
+            title: 'Express Note Taker',
+            link: 'https://github.com/ProgrammingAStorm/Express-Note-Taker',
+            image: require('../../assets/images/express-note-taker.jpg'),
+            deploy: 'https://thawing-oasis-83429.herokuapp.com/notes'
+        },
+        {
+            title: 'Pizza Hunt',
+            link: 'https://github.com/ProgrammingAStorm/pizza-hunt',
+            image: require('../../assets/images/pizza-hunt.jpg'),
+            deploy: 'https://boiling-wildwood-23933.herokuapp.com/'
+        },
+        {
+            title: 'Run Buddy',
+            link: 'https://programmingastorm.github.io/RunBuddy/',
+            image: require('../../assets/images/run-buddy.jpg'),
+            deploy: 'https://github.com/ProgrammingAStorm/RunBuddy'
+        },
+        {
+            title: 'TaskmasterPro',
+            link: 'https://github.com/ProgrammingAStorm/TaskmasterPro',
+            image: require('../../assets/images/taskmaster-pro.jpg'),
+            deploy: 'https://programmingastorm.github.io/TaskmasterPro/'
+        },
+    ]
+
+    return (
+        <section>
+            <Grid2 container spacing={1}>
+                {projects.map((project, i) => (
+                    <Project
+                        title={project.title}
+                        link={project.link}
+                        image={project.image}
+                        deploy={project.deploy}
+                    />
+                ))}
+            </Grid2>
+        </section>
     );
 };
---
+++ src/components/Project/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { Card, CardContent, CardHeader, CardMedia, IconButton, Tooltip } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+
+export default function Project(props) {
+    const {
+        title,
+        link,
+        image,
+        deploy
+    } = props;
+
+    return (
+        <Grid2 xs={6}>
+            <Card sx={{ maxWidth: 256 }}>
+                <CardHeader
+                    title={title}
+                    action={
+                        <div>
+                            <Tooltip title='Repo'>
+                                <IconButton
+                                    component="a"
+                                    href={link}
+                                    target="_blank"
+                                >
+                                    <GitHubIcon />
+                                </IconButton>
+                            </Tooltip>
+
+                            <Tooltip title='Deployment'>
+                                <IconButton
+                                    component="a"
+                                    href={deploy}
+                                    target="_blank"
+                                >
+                                    <OpenInNewIcon />
+                                </IconButton>
+                            </Tooltip>
+                        </div>
+                    }
+                />
+
+                <CardContent>
+                    <CardMedia component="img" src={image} alt={title} ></CardMedia>
+                </CardContent>
+            </Card>
+        </Grid2>
+    );
+};
