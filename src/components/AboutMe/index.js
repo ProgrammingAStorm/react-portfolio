00a6155303215a79b0b54fc64a4b272322c81864
Mark Pavel <markpavel02@gmail.com>
2022-08-14 23:31:01 -0400

    Added Header Styling

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,4 +1,4 @@
-import { Card, CardMedia, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import React from 'react';
 import me from '../../assets/images/me.jpg'
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
-import { Box, Paper } from "@mui/material";
+import { Grid, Paper } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import React from "react"
 import Nav from '../Nav';
 
@@ -6,28 +7,42 @@ export default function Header(props) {
     const { content, setContent } = props;
 
     return (
-        <Box sx={[
-            { backgroundColor: '#1956FF' },
-            { display: 'flex' },
-            { alignItems: 'center' },
-            { justifyContent: 'space-around' },
-            { height: '15%' }
-        ]}>
-            <Paper
-                elevation={2}
-                component="h1"
-                style={{ 
-                    padding: '1rem', 
-                    backgroundColor: '#FF1053' 
-                }}
+        <Grid2 container sx={{ backgroundColor: '#1956FF' }}>
+            <Grid2
+                xs={12}
+                sm={6}
+                sx={[
+                    { display: 'flex' },
+                    { alignItems: 'center' },
+                    { justifyContent: 'space-around' }
+                ]}
             >
-                Mark Pavel
-            </Paper>
+                <Paper
+                    elevation={2}
+                    component="h1"
+                    style={{
+                        padding: '1rem',
+                        backgroundColor: '#FF1053'
+                    }}
+                >
+                    Mark Pavel
+                </Paper>
+            </Grid2>
 
-            <Nav
-                content={content}
-                setContent={setContent}
-            />
-        </Box>
+            <Grid2
+                xs={12}
+                sm={6}
+                sx={[
+                    { display: 'flex' },
+                    { alignItems: 'center' },
+                    { justifyContent: 'space-around' }
+                ]}
+            >
+                <Nav
+                    content={content}
+                    setContent={setContent}
+                />
+            </Grid2>
+        </Grid2>
     );
 };
